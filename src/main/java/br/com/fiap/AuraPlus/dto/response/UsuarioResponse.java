c76b9671045aac99d48fa2f03cf8e51701aeaf2a
Matheus Esteves <matheus.mat160805@gmail.com>
2025-11-17 22:44:19 -0300

    adicionando infos de equipe no get usuario

--- src/main/java/br/com/fiap/AuraPlus/dto/response/UsuarioResponse.java
+++ src/main/java/br/com/fiap/AuraPlus/dto/response/UsuarioResponse.java
@@ -1,7 +1,10 @@
 package br.com.fiap.AuraPlus.dto.response;
 
+import br.com.fiap.AuraPlus.model.Equipe;
 import br.com.fiap.AuraPlus.model.Usuario;
 import br.com.fiap.AuraPlus.model.enums.Role;
+
+import java.util.Optional;
 
 public record UsuarioResponse(
         Long id,
@@ -9,16 +12,21 @@ public record UsuarioResponse(
         String email,
         Role role,
         Boolean ativo,
-        String cargo
+        String cargo,
+        Long equipeId,
+        String equipeNome
 ) {
     public static UsuarioResponse from(Usuario usuario) {
+
         return new UsuarioResponse(
                 usuario.getId(),
                 usuario.getNome(),
                 usuario.getEmail(),
                 usuario.getRole(),
                 usuario.getAtivo(),
-                usuario.getCargo()
+                usuario.getCargo(),
+                Optional.ofNullable(usuario.getEquipe()).map(Equipe::getId).orElse(null),
+                Optional.ofNullable(usuario.getEquipe()).map(Equipe::getNomeTime).orElse(null)
         );
     }
 }
